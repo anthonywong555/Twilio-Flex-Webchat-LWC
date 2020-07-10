54f7bacc30008882483794b9706b1189dc400821
Anthony Wong <anthonywong555@gmail.com>
2020-07-10 11:57:05 -0400

    Load Twilio Flex Widget via Static Resources.

--- sfdx/force-app/main/default/lwc/twilioFlexChatWidget/twilioFlexChatWidget.js
+++ sfdx/force-app/main/default/lwc/twilioFlexChatWidget/twilioFlexChatWidget.js
@@ -1,3 +1,34 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { loadScript } from 'lightning/platformResourceLoader';
+import TwilioFlexWebchat from '@salesforce/resourceUrl/TwilioFlexWebchat';
 
-export default class TwilioFlexChatWidget extends LightningElement {}
+export default class TwilioFlexChatWidget extends LightningElement {
+
+  twilioFlexChatWidgetInitialized = false;
+
+  renderedCallback = async () => {
+    if (this.twilioFlexChatWidgetInitialized) {
+        return;
+    } else {
+      try {
+        this.twilioFlexChatWidgetInitialized = true;
+        await loadScript(this, TwilioFlexWebchat + '/twilio-flex-webchat.min.js');
+        this.initializeTwilioFlexWebchat();
+      } catch (e) {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: 'Error loading Twilio Flex Widget',
+            message: e,
+            variant: 'error'
+          })
+        );
+      }
+    }
+  }
+
+  initializeTwilioFlexWebchat() {
+    console.log(`%c hit`, 'color: red; background-color: black');
+    console.log(`Twilio.FlexWebChat ${Twilio.FlexWebChat}`);
+  }
+}
